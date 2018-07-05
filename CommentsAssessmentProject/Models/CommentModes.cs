4c022e098eed1ffa79b49e76c5339ffb4f4bb072
Mark Allum <mark.allum@hotmail.co.uk>
2018-07-05 14:12:42 +0100

    Initial setup

---
+++ CommentsAssessmentProject/Models/CommentModes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentsAssessmentProject.Models
+{
+    public class Comment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(400)]
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime PostedDateTime { get; set; }
+    }
+}
---
+++ CommentsAssessmentProject/Services/DbService.cs
@@ -0,0 +1,16 @@
+﻿using CommentsAssessmentProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentsAssessmentProject.Services
+{
+    public class DbService : DbContext
+    {
+        public DbService(DbContextOptions<DbService> options) : base(options) { }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
