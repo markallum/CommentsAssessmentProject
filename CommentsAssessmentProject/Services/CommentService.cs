a4bd6b2bdf2fb9a70d95cab4a98a2d518dcf38e0
Mark Allum <mark.allum@hotmail.co.uk>
2018-07-05 14:21:20 +0100

    Created DB and created basic comment service

---
+++ CommentsAssessmentProject/Services/CommentService.cs
@@ -0,0 +1,31 @@
+﻿using CommentsAssessmentProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentsAssessmentProject.Services
+{
+    public interface ICommentsService
+    {
+        Task<List<Comment>> GetComments();
+    }
+
+    public class CommentService : ICommentsService
+    {
+        private DbService _dbService;
+
+        public CommentService(DbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task<List<Comment>> GetComments()
+        {
+            List<Comment> Comments = await _dbService.Comments.OrderByDescending(x => x.PostedDateTime).ToListAsync();
+            return Comments;
+        }
+
+    }
+}
