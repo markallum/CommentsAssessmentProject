6394beee09f2b6b4e87f19171f81385e1e8dae96
Mark Allum <mark.allum@hotmail.co.uk>
2018-07-05 13:55:04 +0100

    Add project files.

---
+++ CommentsAssessmentProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CommentsAssessmentProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
